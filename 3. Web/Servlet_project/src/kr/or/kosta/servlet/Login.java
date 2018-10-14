bfe4d242ffa3d6396bbd2110f50a26e71d28132a
Chicpork <dkgkrmfo@gmail.com>
2018-10-14 23:46:31 +0900

    login ex

---
+++ 3. Web/Servlet_project/src/kr/or/kosta/servlet/Login.java
@@ -0,0 +1,41 @@
+package kr.or.kosta.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String userId = request.getParameter("userid");
+		String userPw = request.getParameter("userpw");
+		response.addCookie(new Cookie("userId", userId));
+		response.addCookie(new Cookie("userPw", userPw));
+		
+		response.sendRedirect("/servlet/");
+	}
+}
